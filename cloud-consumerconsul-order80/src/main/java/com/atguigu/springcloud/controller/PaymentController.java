daa396b6536ef374a861bd00d2cdca0fc8041a01
huiqiulilili <2825084776@qq.com>
2022-02-13 22:06:28 +0800

    服务注册与发现

---
+++ cloud-consumerconsul-order80/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -0,0 +1,27 @@
+package com.atguigu.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+import java.util.UUID;
+
+@RestController
+@Slf4j
+public class PaymentController {
+
+    public static final String INVOKE_URL = "http://consul-provider-payment";
+
+    @Resource
+    private RestTemplate restTemplate;
+
+    @GetMapping("/consumer/consul")
+    public String paymentInfo() {
+        String result = restTemplate.getForObject(INVOKE_URL+"/payment/consul",String.class);
+        return result;
+    }
+}
